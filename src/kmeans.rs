cd0e9527a696e9b34d293b80b8f34d1262627ddd
Ian Brault <ian.brault@engineering.ucla.edu>
2019-02-04 11:59:51 -0800

    Full implementation of k-means clustering, rename KVector to GenericVector.

--- src/kmeans.rs
+++ src/kmeans.rs
@@ -5,27 +5,49 @@
  */
 
 use std::cmp;
-use std::fmt::Debug;
 
 use rand::prelude::*;
 use rand::distributions::{Uniform, WeightedIndex};
 
 // generic vector used in the k-means clustering algorithm
-pub trait KVector<Element=Self>: Clone + Debug {
-    fn distance(&self, other: &Element) -> u32;
+pub trait GenericVector<Element=Self>: Clone {
     fn average(vectors: &Vec<Element>) -> Element;
+    fn distance(&self, other: &Element) -> u32;
 }
 
-fn update_weights<KV>(weights: Vec<u32>, vec: &KV, data: &Vec<KV>) -> Vec<u32> where KV: KVector {
-    let mut new_weights = Vec::with_capacity(weights.len());
-    for (v, w) in data.iter().zip(weights.iter()) {
-        new_weights.push(cmp::min(w, &vec.distance(v)).clone());
+
+// used to store a GenericVector alongside its cluster assignment
+struct ClusterVector<V> where V: GenericVector {
+    assignment: i32,
+    vector: V,
+}
+
+impl<V> ClusterVector<V> where V: GenericVector {
+    fn new(v: V) -> ClusterVector<V> {
+        ClusterVector {
+            assignment: -1,
+            vector: v,
+        }
     }
-    new_weights
+
+    fn from_vectors(data: Vec<V>) -> Vec<ClusterVector<V>> {
+        data.into_iter().map(|v| ClusterVector::new(v)).collect()
+    }
+}
+
+
+fn update_weights<V>(weights: &mut Vec<u32>, vec: &V, data: &Vec<V>)
+    where V: GenericVector
+{
+    for (i, v) in data.iter().enumerate() {
+        weights[i] = cmp::min(weights[i], vec.distance(v));
+    }
 }
 
 // k-means++ implementation
-pub fn k_means_pp<KV>(k: u32, data: &Vec<KV>) -> Vec<KV> where KV: KVector {
+pub fn k_means_pp<V>(k: u32, data: &Vec<V>) -> Vec<V>
+    where V: GenericVector
+{
     let mut centers = Vec::with_capacity(k as usize);
     // the weight for each vector is the minimum distance to a previously-generated center
     let mut weights = vec![std::u32::MAX; data.len()];
@@ -33,23 +55,82 @@ pub fn k_means_pp<KV>(k: u32, data: &Vec<KV>) -> Vec<KV> where KV: KVector {
     let mut rng = thread_rng();
     let udist = Uniform::new(0, data.len());
 
-    let initial = data.get(rng.sample(udist)).unwrap().clone();
-    weights = update_weights(weights, &initial, data);
+    let initial = data[rng.sample(udist)].clone();
+    update_weights(&mut weights, &initial, data);
     centers.push(initial);
 
     for _ in 1..k {
         let dist = WeightedIndex::new(&weights).unwrap();
-        let center = data.get(dist.sample(&mut rng)).unwrap().clone();
-        weights = update_weights(weights, &center, data);
+        let center = data[dist.sample(&mut rng)].clone();
+        update_weights(&mut weights, &center, data);
         centers.push(center);
     }
 
     centers
 }
 
+
+fn min_index(vec: &Vec<i64>) -> usize {
+    let mut min = std::i64::MAX;
+    let mut min_index: i32 = -1;
+
+    for (i, v) in vec.iter().enumerate() {
+        if v < &min {
+            min = *v;
+            min_index = i as i32;
+        }
+    }
+
+    min_index as usize
+}
+
+fn assign_centers<V>(centers: &Vec<V>, cluster_vecs: &mut Vec<ClusterVector<V>>) -> bool
+    where V: GenericVector
+{
+    let mut change_made = false;
+    for cv in cluster_vecs.iter_mut() {
+        let closest = min_index(&centers.iter().map(|c| cv.vector.distance(c) as i64).collect()) as i32;
+
+        if cv.assignment != closest {
+            cv.assignment = closest;
+            change_made = true;
+        }
+    }
+
+    change_made
+}
+
+fn update_centers<V>(centers: Vec<V>, cluster_vecs: &Vec<ClusterVector<V>>) -> Vec<V>
+    where V: GenericVector
+{
+    let n_centers = centers.len();
+    let mut new_centers = Vec::with_capacity(n_centers);
+
+    for i in 0..n_centers {
+        let cluster = cluster_vecs.iter()
+            .filter(|cv| cv.assignment == i as i32)
+            .map(|cv| cv.vector.clone())
+            .collect();
+        new_centers.push(V::average(&cluster));
+    }
+
+    new_centers
+}
+
 // k-means clustering implementation
-pub fn k_cluster<KV>(k: u32, data: Vec<KV>) where KV: KVector {
-    let centroids = k_means_pp(k, &data);
+pub fn k_cluster<V>(k: u32, data: Vec<V>) -> Vec<V>
+    where V: GenericVector
+{
+    let mut centers = k_means_pp(k, &data);
+    let mut cluster_vecs = ClusterVector::from_vectors(data);
+
+    let mut change_made = true;
+    while change_made {
+        change_made = assign_centers(&centers, &mut cluster_vecs);
+        centers = update_centers(centers, &cluster_vecs);
+    }
+
+    centers
 }
 
 
@@ -66,12 +147,12 @@ mod tests {
         ];
         let mut weights = vec![std::u32::MAX; data.len()];
 
-        let center1 = data.get(1).unwrap();
-        weights = update_weights(weights, center1, &data);
+        let center1 = &data[1];
+        update_weights(&mut weights, center1, &data);
         assert_eq!(weights, vec![77, 0, 269, 1456, 321]);
 
-        let center2 = data.get(3).unwrap();
-        weights = update_weights(weights, center2, &data);
+        let center2 = &data[3];
+        update_weights(&mut weights, center2, &data);
         assert_eq!(weights, vec![77, 0, 269, 0, 321]);
     }
 }
--- src/pixel.rs
+++ src/pixel.rs
@@ -4,9 +4,11 @@
  * author: Ian Brault <ian.brault@engineering.ucla.edu>
  */
 
-use crate::kmeans::KVector;
+use std::ops::Mul;
 
-#[derive(Clone, Debug, PartialEq)]
+use crate::kmeans::GenericVector;
+
+#[derive(Clone, Debug, Default, PartialEq)]
 pub struct Pixel {
     r: u8,
     g: u8,
@@ -19,23 +21,37 @@ impl Pixel {
     }
 }
 
-// for use in k-means clustering
-impl KVector for Pixel {
+impl Mul<f64> for Pixel {
+    type Output = Pixel;
+
+    fn mul(self, rhs: f64) -> Pixel {
+        Pixel {
+            r: ((self.r as f64) * rhs) as u8,
+            g: ((self.g as f64) * rhs) as u8,
+            b: ((self.b as f64) * rhs) as u8,
+        }
+    }
+}
+
+impl GenericVector for Pixel {
+    fn average(vectors: &Vec<Pixel>) -> Pixel {
+        let mut sum: (u64, u64, u64) = (0, 0, 0);
+        for v in vectors {
+            sum.0 += v.r as u64;
+            sum.1 += v.g as u64;
+            sum.2 += v.b as u64;
+        }
+
+        let n = vectors.len() as u64;
+        Pixel::new(sum.0 as u8, sum.1 as u8, sum.2 as u8) * (1.0 / n as f64)
+    }
+
     fn distance(&self, other: &Pixel) -> u32 {
         // cast up to i64 to avoid unsigned overflow
         let r_dist = (self.r as i64) - (other.r as i64);
         let g_dist = (self.g as i64) - (other.g as i64);
         let b_dist = (self.b as i64) - (other.b as i64);
         (r_dist*r_dist + g_dist*g_dist + b_dist*b_dist) as u32
-    }
-
-    fn average(vectors: &Vec<Pixel>) -> Pixel {
-        let sum: (u64, u64, u64) = (0, 0, 0);
-        let sum = vectors.iter()
-            .fold(sum, |acc, p| (acc.0 + (p.r as u64), acc.1 + (p.g as u64), acc.2 + (p.b as u64)));
-
-        let n_vecs = vectors.len() as u64;
-        Pixel::new((sum.0 / n_vecs) as u8, (sum.1 / n_vecs) as u8, (sum.2 / n_vecs) as u8)
     }
 }
 
@@ -51,13 +67,5 @@ mod tests {
         assert_eq!(p.distance(&Pixel::new(8, 5, 18)), 21);
         assert_eq!(p.distance(&Pixel::new(16, 3, 22)), 21);
         assert_eq!(p.distance(&Pixel::new(12, 4, 20)), 0);
-    }
-
-    #[test]
-    fn test_average() {
-        let data = vec![
-            Pixel::new(0,0,0), Pixel::new(4,5,6), Pixel::new(12,8,20), Pixel::new(16,1,42)
-        ];
-        assert_eq!(Pixel::average(&data), Pixel::new(8, 3, 17));
     }
 }
